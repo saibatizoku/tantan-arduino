716f549fce7742379e5c74a332d3fe19cae4daa2
Joaquin R <globojorro@gmail.com>
2014-08-09 22:33:36 -0500

    Archivos iniciales del Nodo Acuático para 4 estanques

---
+++ NODO_ACUATICO/NODO_ACUATICO.ino
@@ -0,0 +1,32 @@
+/*
+   Copyright 2014 Óscar Justo, Joaquín Rosales
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ 
+ Programa para un nodo acuático con:
+ 1 sensor de pH
+ 4 sensores de oxígeno disuelto
+ 4 sensores de temperatura
+ 
+ Xalapa, Veracruz. México 2014
+
+ */
+
+void setup () {
+}
+
+void loop () {
+}
+
