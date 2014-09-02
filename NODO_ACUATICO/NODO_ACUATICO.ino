fbe2c25059576d8c9fc8b76da63b2d1457a12b3c
Joaquin R <globojorro@gmail.com>
2014-09-02 14:51:37 -0500

    Cambios a lo bruto

--- NODO_ACUATICO/NODO_ACUATICO.ino
+++ NODO_ACUATICO/NODO_ACUATICO.ino
@@ -21,12 +21,437 @@
  4 sensores de temperatura
  
  Xalapa, Veracruz. México 2014
+*/
+ 
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <SoftwareSerial.h>
+#include <MenuSystem.h>
+#include <LiquidCrystal.h>
+#include <TanTan.h>
 
- */
+#define ONE_WIRE_BUS 23
 
-void setup () {
+#define RX_PH 50
+#define TX_PH 51
+
+#define RX_OD_1 10
+#define TX_OD_1 6
+
+#define RX_OD_2 11
+#define TX_OD_2 7
+
+#define RX_OD_3 12
+#define TX_OD_3 8
+
+#define RX_OD_4 13
+#define TX_OD_4 9
+
+SoftwareSerial pHserial(RX_PH, TX_PH);
+SoftwareSerial OD_1serial(RX_OD_1, TX_OD_1);
+SoftwareSerial OD_2serial(RX_OD_2, TX_OD_2);
+SoftwareSerial OD_3serial(RX_OD_3, TX_OD_3);
+SoftwareSerial OD_4serial(RX_OD_4, TX_OD_4);
+
+
+int W_BUTTON = 18;
+int S_BUTTON = 19;
+int A_BUTTON = 2;
+int D_BUTTON = 3;
+
+// Menu variables
+MenuSystem ms;
+Menu mm("Info estanques");
+MenuItem mm_mi1("Medir pH");
+MenuItem mm_mi2("Medir OD1");
+MenuItem mm_mi3("Medir OD2");
+MenuItem mm_mi4("Medir OD3");
+MenuItem mm_mi5("Medir OD4");
+Menu mu1("Modo Calibracion");
+MenuItem mu1_mi1("Sensor pH");
+MenuItem mu1_mi2("OD 1");
+MenuItem mu1_mi3("OD 2");
+MenuItem mu1_mi4("OD 3");
+MenuItem mu1_mi5("OD 4");
+
+LiquidCrystal lcd(30,31,32,33,34,35);
+
+//Variable de inicio
+boolean inicial = true;
+Nodo nodo;
+volatile boolean BOTON_OPRIMIDO = false;
+volatile int BOTON_NOMBRE;
+
+void on_item1_selected(MenuItem* p_menu_item)
+{
+  print_pH();
 }
 
-void loop () {
+void print_pH()
+{
+  pHserial.listen();
+  float pHLect = nodo.read_pH();
+  lcd.setCursor(0,1);
+  char tempph[5];
+  String pHs = dtostrf(pHLect, 1, 2, tempph);
+  //  String OD1s = dtostrf(OD1, 1, 2, tempf);
+  String msg = "pH:" + pHs;
+  Serial.println(msg);
+  msg = "pH: " + pHs + "   ";
+  lcd.print(msg);
+  delay(1500); // so we can look the result on the LCD
 }
+
+void on_item2_selected(MenuItem* p_menu_item)
+{
+  print_OD1();
+}
+
+void print_OD1()
+{
+  OD_1serial.listen();
+  float OD1 = nodo.read_OD1();
+  char tempod1[5];
+  String OD1s = dtostrf(OD1, 1, 2, tempod1);
+  lcd.setCursor(0,1);
+  String msg = "OD1:" + OD1s;
+  Serial.println(msg);
+  msg = "OD1: " + OD1s + "  " ;
+  lcd.print(msg);
+  delay(3000); // so we can look the result on the LCD
+}
+
+void on_item3_selected(MenuItem* p_menu_item)
+{
+  print_OD2();
+}
+
+void print_OD2()
+{
+  OD_2serial.listen();
+  float OD2 = nodo.read_OD2();
+  char tempod1[5];
+  String OD2s = dtostrf(OD2, 1, 2, tempod1);
+  lcd.setCursor(0,1);
+  String msg = "OD2:" + OD2s;
+  Serial.println(msg);
+  msg = "OD2: " + OD2s + "  " ;
+  lcd.print(msg);
+  delay(3000); // so we can look the result on the LCD
+}
+
+void on_item4_selected(MenuItem* p_menu_item)
+{
+  print_OD3();
+}
+
+void print_OD3()
+{
+  OD_3serial.listen();
+  float OD3 = nodo.read_OD3();
+  char tempod1[5];
+  String OD3s = dtostrf(OD3, 1, 2, tempod1);
+  lcd.setCursor(0,1);
+  String msg = "OD3:" + OD3s;
+  Serial.println(msg);
+  msg = "OD3: " + OD3s + "  " ;
+  lcd.print(msg);
+  delay(3000); // so we can look the result on the LCD
+}
+
+void on_item5_selected(MenuItem* p_menu_item)
+{
+  print_OD4();
+}
+
+void print_OD4()
+{
+  OD_4serial.listen();
+  float OD4 = nodo.read_OD4();
+  char tempod1[5];
+  String OD4s = dtostrf(OD4, 1, 2, tempod1);
+  lcd.setCursor(0,1);
+  String msg = "OD4:" + OD4s;
+  Serial.println(msg);
+  msg = "OD4: " + OD4s + "  " ;
+  lcd.print(msg);
+  delay(3000); // so we can look the result on the LCD
+}
+
+void on_cal1_selected(MenuItem* p_menu_item)
+{
+  lcd.setCursor(0,1);
+  lcd.print("Cal pH @4  ");
+  delay(1500);
+  lcd.setCursor(0,1);
+  lcd.print("Cal pH @7  ");
+  delay(1500);
+  lcd.setCursor(0,1);
+  lcd.print("Cal pH @10 ");
+  delay(1500);
+}
+
+void on_cal2_selected(MenuItem* p_menu_item)
+{
+  lcd.setCursor(0,1);
+  lcd.print("Cal OD 1  ");
+  delay(1500); // so we can look the result on the LCD
+}
+
+void on_cal3_selected(MenuItem* p_menu_item)
+{
+  lcd.setCursor(0,1);
+  lcd.print("Cal OD 2  ");
+  delay(1500); // so we can look the result on the LCD
+}
+
+void on_cal4_selected(MenuItem* p_menu_item)
+{
+  lcd.setCursor(0,1);
+  lcd.print("Cal OD 3  ");
+  delay(1500); // so we can look the result on the LCD
+}
+
+void on_cal5_selected(MenuItem* p_menu_item)
+{
+  lcd.setCursor(0,1);
+  lcd.print("Cal OD 4  ");
+  delay(1500); // so we can look the result on the LCD
+}
+
+void inicializar_botones() {
+  attachInterrupt(0, ISR_S, CHANGE);
+  attachInterrupt(1, ISR_W, CHANGE);
+  attachInterrupt(4, ISR_D, CHANGE);
+  attachInterrupt(5, ISR_A, CHANGE);
+}
+
+void ISR_W()
+{
+  BOTON_OPRIMIDO = true;
+  BOTON_NOMBRE = 'w';
+}
+void ISR_S()
+{
+  BOTON_OPRIMIDO = true;
+  BOTON_NOMBRE = 's';
+}
+void ISR_A()
+{
+  BOTON_OPRIMIDO = true;
+  BOTON_NOMBRE = 'a';
+}
+void ISR_D()
+{
+  BOTON_OPRIMIDO = true;
+  BOTON_NOMBRE = 'd';
+}
+
+void inicializar_seriales() {
+  Serial.begin(38400);
+  Serial.println("Iniciando...");
+  pHserial.begin(38400);
+  Serial.println("pH...");
+  OD_1serial.begin(38400);
+  Serial.println("OD 1...");
+  OD_2serial.begin(38400);
+  Serial.println("OD 2...");
+  OD_3serial.begin(38400);
+  Serial.println("OD 3...");
+  OD_4serial.begin(38400);
+  Serial.println("OD 4...");
+  if (inicial) {
+    modo_standby();
+    inicial=false;
+  }
+}
+void modo_standby() {
+  OD_1serial.print("e\r");
+  delay(50);
+  OD_1serial.print("e\r");
+
+  OD_2serial.print("e\r");
+  delay(50);
+  OD_2serial.print("e\r");
+
+  OD_3serial.print("e\r");
+  delay(50);
+  OD_3serial.print("e\r");
+
+  OD_4serial.print("e\r");
+  delay(50);
+  OD_4serial.print("e\r");
+
+  pHserial.print("e\r");
+  delay(50);
+  pHserial.print("e\r");
+
+}
+void setup()
+{
+  inicializar_botones();
+  inicializar_seriales();
+  nodo.add_pH(pHserial);
+  nodo.add_OD1(OD_1serial);
+  nodo.add_OD2(OD_2serial);
+  nodo.add_OD3(OD_3serial);
+  nodo.add_OD4(OD_4serial);
+  //Serial.begin(SERIAL_BAUD);
+  lcd.begin(16, 2);
+
+  serialPrintHelp();
+  Serial.println("Construyendo menu.");
+  // Menu setup
+  /*
+       Menu Structure:
+   -Estanque1
+   --pH
+   -Estanque2
+   -Estanque3
+   -Estanque4
+   -Calibrar
+   --pH
+   --OD1
+   --OD2
+   --OD3
+   --OD4   
+   */
+  mm.add_item(&mm_mi1, &on_item1_selected);
+  mm.add_item(&mm_mi2, &on_item2_selected);
+  mm.add_item(&mm_mi3, &on_item3_selected);
+  mm.add_item(&mm_mi4, &on_item4_selected);
+  mm.add_item(&mm_mi5, &on_item5_selected);
+  mm.add_menu(&mu1);
+  mu1.add_item(&mu1_mi1, &on_cal1_selected);
+  mu1.add_item(&mu1_mi2, &on_cal2_selected);
+  mu1.add_item(&mu1_mi3, &on_cal3_selected);
+  mu1.add_item(&mu1_mi4, &on_cal4_selected);
+  mu1.add_item(&mu1_mi5, &on_cal5_selected);
+  ms.set_root_menu(&mm);
+  Serial.println("Menu construido.");
+  displayMenu();
+  print_all();
+  displayMenu();
+}
+
+void print_all()
+{
+  print_pH();
+  print_OD1();
+  print_OD2();
+  print_OD3();
+  print_OD4();
+}
+
+void loop()
+{
+  // Handle serial commands
+  buttonHandler();
+  serialHandler();
+  // Wait for two seconds so the output is viewable
+  delay(1000);
+}
+
+void buttonHandler() {
+  if (BOTON_OPRIMIDO) {
+    BOTON_OPRIMIDO = false;
+//    Serial.print("Boton ");
+//    Serial.println((char)BOTON_NOMBRE);
+    switch((char)BOTON_NOMBRE) {
+    case 'w': // Previus item
+      ms.prev();
+      displayMenu();
+      break;
+    case 's': // Next item
+      ms.next();
+      displayMenu();
+      break;
+    case 'a': // Back presed
+      ms.back();
+      displayMenu();
+      break;
+    case 'd': // Select presed
+      ms.select();
+      displayMenu();
+      break;
+    }
+  }
+}
+
+void displayMenu() {
+  lcd.clear();
+  lcd.setCursor(0,0);
+  // Display the menu
+  Menu const* cp_menu = ms.get_current_menu();
+
+  //lcd.print("Current menu name: ");
+  lcd.print(cp_menu->get_name());
+
+  lcd.setCursor(0,1);
+
+  lcd.print(cp_menu->get_selected()->get_name());
+}
+
+void serialHandler() {
+  char inChar;
+  if((inChar = Serial.read())>0) {
+    switch (inChar) {
+    case 'w': // Previus item
+      ms.prev();
+      displayMenu();
+      break;
+    case 's': // Next item
+      ms.next();
+      displayMenu();
+      break;
+    case 'a': // Back presed
+      ms.back();
+      displayMenu();
+      break;
+    case 'd': // Select presed
+      ms.select();
+      displayMenu();
+      break;
+    case '0': // pH
+      print_pH();
+      displayMenu();
+      break;
+    case '1': // pH
+      print_OD1();
+      displayMenu();
+      break;
+    case '2': // pH
+      print_OD2();
+      displayMenu();
+      break;
+    case '3': // pH
+      print_OD3();
+      displayMenu();
+      break;
+    case '4': // pH
+      print_OD4();
+      displayMenu();
+      break;
+    case '?':
+    case 'h': // Display help
+      serialPrintHelp();
+      break;
+    default:
+      break;
+    }
+  }
+}
+
+void serialPrintHelp() {
+  Serial.println("***************");
+  Serial.println("w: go to previus item (up)");
+  Serial.println("s: go to next item (down)");
+  Serial.println("a: go back (right)");
+  Serial.println("d: select \"selected\" item");
+  Serial.println("?: print this help");
+  Serial.println("h: print this help");
+  Serial.println("***************");
+}
+
+
 
